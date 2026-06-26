1d8a93af9958ab62f737fb6e72d3f2ab822254cf
seoyoungkim <ksy153019@gmail.com>
2026-06-26 18:55:00 +0200

    book project with genre

--- ConsoleApp-Seoyoung/Book.cs
+++ ConsoleApp-Seoyoung/Book.cs
@@ -2,20 +2,23 @@ namespace ConsoleApp_Seoyoung;
 
 public class Book
 {
-    private int Id{get;set;}
-    public string Isbn {get;set;}
-    public string Title{get;set;}
-    public string Author{get;set;}
+    //private int Id{get;set;} 
+    public int Id { get; private set; }
+    public string Isbn {get; private set;} 
+    public string Title{get;set;} 
+    public string Author{get;set;} 
     public double Price{get;set;}
-    
+    public Genre Genre{get;set;}
+            
 
-    public Book(int id, string isbn, string title,string author,double price)
+    public Book(int id, string isbn, string title, string author, double price,Genre genre)
     {
-        this.Id = id;
-        this.Isbn = isbn;
-        this.Title = title;
-        this.Author = author;
+        this.Id = id; 
+        this.Isbn = isbn; 
+        this.Title = title; 
+        this.Author = author; 
         this.Price = price;
-    }
-
+        this.Genre = genre;
+        
+    } 
 }
--- ConsoleApp-Seoyoung/Program.cs
+++ ConsoleApp-Seoyoung/Program.cs
@@ -9,15 +9,23 @@ public class Program
 
     public static void Main(string[] args)
     {
-        Book book1 = new Book(1,"123-456789","Harry Potter","J.K.Rowling",19.99);
-        Book book2 = new Book(2,"987-654321","Project Hailmary","Andy Weir",13.99);
+        Book book1 = new Book(1,"123-456789","Harry Potter","J.K.Rowling",19.99, Genre.Fiction);
+        Book book2 = new Book(2,"987-654321","Project Hailmary","Andy Weir",13.99,Genre.Fiction);
+        Book book3 = new Book(3, "112-23344", "Sherlock Holmes","Arthur Conan Doyle",20.99,Genre.Crime);
         
         Console.WriteLine(book1.Title);
         Console.WriteLine(book2.Title);
+        Console.WriteLine(book3.Title);
         Console.WriteLine(book1.Author);
         Console.WriteLine(book2.Author);
+        Console.WriteLine(book3.Author);
         Console.WriteLine(book1.Price);
         Console.WriteLine(book2.Price);
+        Console.WriteLine(book3.Price);
+        Console.WriteLine(book1.Genre);
+        Console.WriteLine(book2.Genre);
+        Console.WriteLine(book3.Genre);
+        
 
     }
 }
